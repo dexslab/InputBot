302dacd6d766dea4d7c0e30fa815722fd3554aec
Dex's Lab <dex35803@gmail.com>
2025-09-23 19:56:29 -0400

    update fixes

--- src/public.rs
+++ src/public.rs
@@ -92,7 +92,7 @@ fn mouse_canonical_names_lower() -> &'static HashMap<String, MouseButton> {
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone, EnumIter)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize, Debug))]
+#[cfg_attr(feature = "serde", derive(Serialize, Debug))]
 pub enum KeybdKey {
     BackspaceKey,
     TabKey,
